87f5ec86f199c70dcdce859d8728d20bf728a13b
AdemarCastro <2022002444@ifam.edu.br>
2024-01-16 11:43:03 -0400

    [STYLES] - Adiciona uma nova organização de estilização ao componente 'qualification'. Altera informações exibidas

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -84,7 +84,7 @@ const Qualification = () => {
             {/* 1ª - data */}
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title left" id="data1">Soldado EV</h3>
+                <h3 className="qualification__title left" id="data1">Soldado</h3>
                 <span className="qualification__subtitle left" id="data1">PQR MNT 12 - Manaus/AM</span>
                 <div className="qualification__calender">
                   <i className="uil uil-calendar-alt left" id="data1"> 03/2017 - 02/2018</i>
@@ -106,10 +106,74 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="qualification__title right" id="data2">Bolsista</h3>
-                <span className="qualification__subtitle right" id="data2">Projeto Aranouá - Manaus/AM</span>
+                <h3 className="qualification__title right" id="data2">Bolsista PAPE</h3>
+                <span className="qualification__subtitle right" id="data2">ARANOUÁ SAMSUNG - Manaus/AM</span>
                 <div className="qualification__calender">
                   <i className="uil uil-calendar-alt right" id="data2"> 07/2022 - Agora</i>
+                </div>
+              </div>
+            </div>
+            {/* 3ª - data */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title left" id="data3">Aluno Web Academy</h3>
+                <span className="qualification__subtitle left" id="data3">IComp/UFAM - Manaus/AM</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt left" id="data3"> 08/2023 - 09/2023</i>
+                </div>
+              </div>
+
+                <div>
+                  <span className="qualification__rounder" id="data3"></span>
+                  <span className="qualification__line" id="data3"></span>
+                </div>
+            </div>
+            {/* 4ª - data */}
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                  <span className="qualification__rounder" id="data4"></span>
+                  <span className="qualification__line" id="data4"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title right" id="data4">Estagiário Desenvolvedor de Software</h3>
+                <span className="qualification__subtitle right" id="data4">DTI-PMAM - Manaus/AM</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt right" id="data4"> 09/2023 - Agora</i>
+                </div>
+              </div>
+            </div>
+            {/* 5ª - data */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title left" id="data5">Bolsista Full Stack</h3>
+                <span className="qualification__subtitle left" id="data5">Senac AM - Manaus/AM</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt left" id="data5"> 10/2023 - Agora</i>
+                </div>
+              </div>
+
+                <div>
+                  <span className="qualification__rounder" id="data5"></span>
+                  <span className="qualification__line" id="data5"></span>
+                </div>
+            </div>
+            {/* 6ª - data */}
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                  <span className="qualification__rounder" id="data6"></span>
+                  <span className="qualification__line" id="data6"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title right" id="data6">Bolsista Web Academy</h3>
+                <span className="qualification__subtitle right" id="data6">IComp/UFAM - Manaus/AM</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt right" id="data6"> 02/2024 - Agora</i>
                 </div>
               </div>
             </div>
